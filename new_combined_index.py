6979bb85afcd7cbc1545ec68d2be49125bc2cb79
Greta Graeler <86931280+GGraeler@users.noreply.github.com>
2021-10-18 10:27:32 -0700

    Update new_combined_index.py

--- new_combined_index.py
+++ new_combined_index.py
@@ -7,11 +7,14 @@ import pandas as pd
 csv_file_directory = '/usr1/home/nas-qnap/MRMS/2016_Tracks'
 count = 0
 
+#For-loop creates a new 'Match' column to check whether or not 
 for file_n in sorted(os.listdir(csv_file_directory)):
+	#prints file, puts file to pandas dataframe, makes ID column into a list for sorting
 	print(file_n)
 	df_day = pd.read_csv(csv_file_directory+'/'+file_n)
 	string_column = df_day['ID'].to_list()
 	
+	#Creates a T/F list. If the index doesn't match the number before, False is appended. If it matches, True is appended.
 	match_list = []
 	for column in range(1, len(string_column)):
 		if column == 1:
@@ -21,11 +24,15 @@ for file_n in sorted(os.listdir(csv_file_directory)):
 		else:
 			match_list.append('True')
 		gc.collect()
+		
+	#Creates a new column 'Match' in pandas dataframe
 	df_day['Match'] = match_list
 	
+	#Replaces old csv file with new csv with 'Match' column
 	df_day.to_csv(csv_file_directory+'/'+file_n, index = False)
 	gc.collect()
 
+#For-loop makes unique indexes for each csv file
 for file_n in sorted(os.listdir(csv_file_directory)):
 	print(file_n)
 	df_day = pd.read_csv(csv_file_directory+'/'+file_n)
